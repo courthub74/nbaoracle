7f984e0bec7ae3042de25b863d430725c1f6111c
Courdevelops <courdevelops@gmail.com>
2022-02-24 18:14:41 -0500

    allteams

--- nbaproject/settings.py
+++ nbaproject/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-xvulp-ffdgbsr_(h#mrl8r_bm$9)k%ub_(ytj0a&o=^c&6(64i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
